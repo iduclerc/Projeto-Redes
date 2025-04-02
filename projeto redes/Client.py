c221b144824ac6c56989e9fe8c501d153895c39d
luis perylo <lfpb@cesar.school>
2025-04-02 10:46:59 -0300

    entrega 01

--- projeto redes/Client.py
+++ projeto redes/Client.py
@@ -4,15 +4,16 @@ def start_client(server_ip='localhost', port=5050):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, port))
 
-    modo_operacao = "lote" 
-    tamanho_max = "100"     
+    modo_operacao = input("[Client] Escolha o modo de operação (individual/lote): ").strip().lower()
+    tamanho_max = input("[Client] Defina o tamanho máximo da mensagem: ").strip()
 
     handshake = f"{modo_operacao}|{tamanho_max}"
+
     client_socket.send(handshake.encode())
-    print(f"[Cliente] Handshake enviado: {handshake}")
+    print(f"[Client] Handshake enviado: {handshake}")
 
     resposta = client_socket.recv(1024).decode()# para receber a confirmação da ação .
-    print(f"[Cliente] Resposta do servidor: {resposta}")
+    print(f"[Client] Resposta do servidor: {resposta}")
 
     client_socket.close()
 
